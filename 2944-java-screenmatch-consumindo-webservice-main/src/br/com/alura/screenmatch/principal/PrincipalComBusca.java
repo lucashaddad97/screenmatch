3c233d20576b7b002322b8532e2814a36a0ab524
Lucas Haddad <145071356+lucashaddad97@users.noreply.github.com>
2026-04-10 22:59:55 -0300

    principalbusca

--- 2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ 2944-java-screenmatch-consumindo-webservice-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,20 +1,42 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite filme para busca: ");
+        var busca = leitura.nextLine();
+        String endereco = "https://www.omdbapi.com/?i=" + busca +"&apikey=d38c455f" ;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt0106062&apikey=d38c455f"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request,HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+//        Titulo meutitulo = gson.fromJson(response.body(), Titulo.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(response.body(), TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println(meuTitulo);
+
 
     }
 }
